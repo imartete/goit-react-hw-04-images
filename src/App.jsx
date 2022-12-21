e8392c7a8caf2c925a6ea3423c4ea2a5a4e649f0
Marta <maardiakiv@gmail.com>
2022-12-21 18:09:55 +0100

    display gallery with fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import fetchPictures from 'utils/api';
+import { fetchPictures } from 'utils/api';
 import Searchbar from 'components/Searchbar/Searchbar';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 
 export class App extends React.Component {
   state = {
@@ -8,31 +9,37 @@ export class App extends React.Component {
     isLoading: false,
     error: null,
     searchQuery: '',
+    page: 1,
   };
-
-  async componentDidMount() {
-    this.setState({ isLoading: true });
-
-    try {
-      const pictures = await fetchPictures('swag', 1);
-      this.setState({ pictures });
-    } catch (error) {
-      this.setState({ error });
-      console.log(this.state.error);
-      //TODO:
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
 
   registerSearchQuery = value => {
     this.setState({ searchQuery: value });
   };
 
+  async componentDidUpdate(prevProps, prevState) {
+    if (this.state.searchQuery !== prevState.searchQuery) {
+      this.setState({ isLoading: true });
+      console.log('Component Updated');
+      try {
+        const pictures = await fetchPictures(
+          this.state.searchQuery,
+          this.state.page
+        );
+        this.setState({ pictures });
+      } catch (error) {
+        this.setState({ error });
+        console.log(this.state.error);
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
   render() {
     return (
       <div>
         <Searchbar onSubmit={this.registerSearchQuery} />
+        <ImageGallery images={this.state.pictures} />
       </div>
     );
   }
