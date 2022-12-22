6b8d17461a0a8b5610eecc662877c2580b70b105
Marta <maardiakiv@gmail.com>
2022-12-22 16:54:31 +0100

    portal loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,10 @@
 import 'styles.css';
+import { createPortal } from 'react-dom';
+
+const loaderRoot = document.querySelector('#loader-root');
 
 export function Loader() {
-  return (
+  return createPortal(
     <div className="Overlay">
       <div className="lds-spinner">
         <div></div>
@@ -17,6 +20,7 @@ export function Loader() {
         <div></div>
         <div></div>
       </div>
-    </div>
+    </div>,
+    loaderRoot
   );
 }
