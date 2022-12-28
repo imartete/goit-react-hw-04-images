79d1294291112ea73f2ea82f2048fda18faa937d
Marta <maardiakiv@gmail.com>
2022-12-28 23:43:11 +0100

    rewrite with hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import usePrevious from 'hooks/usePrevious';
 import { fetchPictures } from 'utils/api';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
@@ -9,109 +11,92 @@ import { Section } from 'components/Section/Section';
 import { Notification } from 'components/Notification/Notification';
 import { Error } from 'components/Error/Error';
 
-export class App extends React.Component {
-  state = {
-    pictures: [],
-    isLoading: false,
-    error: '',
-    searchQuery: '',
-    page: 1,
-    picture: null,
-    totalPages: null,
-  };
+export function App() {
+  const [pictures, setPictures] = useState([]);
+  const [picture, setPicture] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchQuery, pictures, page } = this.state;
+  const prevSearchQuery = usePrevious(searchQuery);
 
-    if (searchQuery !== prevState.searchQuery || page !== prevState.page) {
-      this.setState({ isLoading: true });
-      try {
-        const response = await fetchPictures(searchQuery, page);
-        const morePictures = response.hits.map(
-          ({ id, webformatURL, tags, largeImageURL }) => ({
-            id,
-            webformatURL,
-            tags,
-            largeImageURL,
-          })
-        );
-        this.setState({
-          pictures: [...pictures, ...morePictures],
-          error: '',
-          totalPages: response.totalHits,
-        });
-      } catch (error) {
-        this.setState({ error: error.message });
-      } finally {
-        this.setState({ isLoading: false });
-      }
+  useEffect(() => {
+    if (searchQuery === '' && page === 1) return;
+    getPictures();
+  }, [searchQuery, page]);
+
+  async function getPictures() {
+    try {
+      setLoading(true);
+      const response = await fetchPictures(searchQuery, page);
+      const morePictures = response.hits.map(
+        ({ id, webformatURL, tags, largeImageURL }) => ({
+          id,
+          webformatURL,
+          tags,
+          largeImageURL,
+        })
+      );
+      setPictures([...pictures, ...morePictures]);
+      setError('');
+      setTotalPages(response.totalHits);
+    } catch (error) {
+      setError('');
+    } finally {
+      setLoading(false);
     }
   }
 
-  registerSearchQuery = searchQuery => {
-    this.setState(prevState => {
-      if (prevState.searchQuery !== searchQuery) {
-        return { searchQuery, page: 1, pictures: [] };
-      }
-    });
-  };
-
-  handlePagination = () => {
-    this.setState({ page: this.state.page + 1 });
-  };
-
-  handleImageClick = picture => {
-    this.setState({ picture });
-  };
-
-  closeModal = () => {
-    this.setState({
-      picture: null,
-    });
-  };
-
-  render() {
-    const {
-      pictures,
-      isLoading,
-      picture,
-      totalPages,
-      page,
-      searchQuery,
-      error,
-    } = this.state;
-
-    const picturesExist = pictures.length > 0;
-    const notOnLastPage = page < totalPages / 12;
-
-    return (
-      <div>
-        <Searchbar onSubmit={this.registerSearchQuery} />
-        {isLoading && <Loader />}
-        {error && (
-          <Section>
-            <Error message={error} />
-          </Section>
-        )}
-        {!pictures.length && searchQuery && !error && (
-          <Section>
-            <Notification />
-          </Section>
-        )}
-        {picturesExist && (
-          <Section>
-            <ImageGallery images={pictures} getModal={this.handleImageClick} />
-          </Section>
-        )}
-        {picturesExist && notOnLastPage && (
-          <Section>
-            <Button onClick={this.handlePagination}></Button>
-          </Section>
-        )}
-        {picture && (
-          <Modal handleClose={this.closeModal} pictureObj={picture} />
-        )}
-      </div>
-    );
+  function registerSearchQuery(searchQuery) {
+    if (prevSearchQuery !== searchQuery) {
+      setPictures([]);
+      setPage(1);
+      setSearchQuery(searchQuery);
+    }
   }
+
+  function handlePagination() {
+    setPage(page + 1);
+  }
+
+  function handleImageClick(picture) {
+    setPicture(picture);
+  }
+
+  function closeModal() {
+    setPicture(null);
+  }
+
+  const picturesExist = pictures.length > 0;
+  const notOnLastPage = page < totalPages / 12;
+
+  return (
+    <div>
+      <Searchbar onSubmit={registerSearchQuery} />
+      {isLoading && <Loader />}
+      {error && (
+        <Section>
+          <Error message={error} />
+        </Section>
+      )}
+      {!pictures.length && searchQuery && !error && (
+        <Section>
+          <Notification />
+        </Section>
+      )}
+      {picturesExist && (
+        <Section>
+          <ImageGallery images={pictures} getModal={handleImageClick} />
+        </Section>
+      )}
+      {picturesExist && notOnLastPage && (
+        <Section>
+          <Button onClick={handlePagination}></Button>
+        </Section>
+      )}
+      {picture && <Modal handleClose={closeModal} pictureObj={picture} />}
+    </div>
+  );
 }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,72 +1,65 @@
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import 'styles.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class Searchbar extends React.Component {
-  state = {
-    searchQuery: '',
-  };
+export function Searchbar({ onSubmit }) {
+  const [searchQuery, setSearchQuery] = useState('');
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+  function notify() {
+    toast('Please, enter your search keyword!');
+  }
 
-  notify = () => toast('Please, enter your search keyword!');
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
 
-  handleChange = event => {
-    this.setState({
-      searchQuery: event.currentTarget.value,
-    });
-  };
-
-  handleSubmit = event => {
+  function handleSubmit(event) {
     event.preventDefault();
-    if (this.state.searchQuery.trim() === '') {
-      this.notify();
+    if (searchQuery.trim() === '') {
+      notify();
       return;
     }
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: '' });
+    onSubmit(searchQuery);
+    setSearchQuery('');
     event.currentTarget.reset();
-  };
-
-  render() {
-    return (
-      <header className="Searchbar">
-        <form onSubmit={this.handleSubmit} className="SearchForm">
-          <button type="submit" className="SearchForm-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <circle cx="10" cy="10" r="7"></circle>
-              <line x1="21" y1="21" x2="15" y2="15"></line>
-            </svg>
-          </button>
-
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onInput={this.handleChange}
-          />
-        </form>
-        <ToastContainer />
-      </header>
-    );
   }
+
+  return (
+    <header className="Searchbar">
+      <form onSubmit={handleSubmit} className="SearchForm">
+        <button type="submit" className="SearchForm-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <circle cx="10" cy="10" r="7"></circle>
+            <line x1="21" y1="21" x2="15" y2="15"></line>
+          </svg>
+        </button>
+
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onInput={handleChange}
+        />
+      </form>
+      <ToastContainer />
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
