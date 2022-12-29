5b26a7b082d1899e0c16d5ef0fd93902aa930881
Marta <maardiakiv@gmail.com>
2022-12-29 16:13:55 +0100

    rewrite Modal components with hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,46 +1,42 @@
 import React from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import 'styles.css';
 
-export class Modal extends React.Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = event => {
-    if (event.key === 'Escape') {
-      this.props.handleClose();
+export function Modal({ handleClose, pictureObj }) {
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === 'Escape') {
+        handleClose();
+      }
     }
-  };
 
-  static propTypes = {
-    handleClose: PropTypes.func.isRequired,
-    pictureObj: PropTypes.shape({
-      alt: PropTypes.string.isRequired,
-      modalImage: PropTypes.string.isRequired,
-    }).isRequired,
-  };
+    window.addEventListener('keydown', handleKeyDown);
 
-  handleOverlayClick = event => {
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleClose]);
+
+  function handleOverlayClick(event) {
     if (event.currentTarget === event.target) {
-      this.props.handleClose();
+      handleClose();
     }
-  };
-
-  render() {
-    return (
-      <div className="Overlay" onClick={this.handleOverlayClick}>
-        <div className="Modal">
-          <img
-            src={this.props.pictureObj.modalImage}
-            alt={this.props.pictureObj.alt}
-          />
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <div className="Overlay" onClick={handleOverlayClick}>
+      <div className="Modal">
+        <img src={pictureObj.modalImage} alt={pictureObj.alt} />
+      </div>
+    </div>
+  );
 }
+
+Modal.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+  pictureObj: PropTypes.shape({
+    alt: PropTypes.string.isRequired,
+    modalImage: PropTypes.string.isRequired,
+  }).isRequired,
+};
