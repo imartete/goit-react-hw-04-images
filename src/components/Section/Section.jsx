132c45971ca79b000937f71cb6de5ba72cbdc60d
Marta <maardiakiv@gmail.com>
2022-12-29 20:33:32 +0100

    fix section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
 import './Section.modules.css';
 
-export const Section = ({ children }) => {
+export const Section = ({ children, title = 'title' }) => {
   return (
     <section className="section">
+      <h1 className="visually-hidden">{title}</h1>
       <div className="container">{children}</div>
     </section>
   );
 };
 
 Section.propTypes = {
+  title: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
